b91f9b29002c1bde762f91729d4e647858f11f6d
rajchauhan2816 <rajchauhan2816@gmail.com>
2021-08-09 20:00:32 +0530

    changes

---
+++ test.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <string.h>
+#include <vector>
+#define int long long int
+#define endl "\n"
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+using namespace std;
+
+void io() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+void run() {
+    int n=5;
+    cout << "hii";
+}
+
+int32_t main() {
+    FastIO
+    //io();
+    run();
+}
